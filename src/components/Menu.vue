8212ca1fc0101a863b3fe756020be75613c68c56
Juliette Le Meudec <94185070+Bluje@users.noreply.github.com>
2026-03-20 12:10:56 +0100

    Changed to vue version

---
+++ src/components/Menu.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { sections } from '@/stores/sections/section.ts'
+</script>
+
+<template>
+  <div>
+    <a v-for="section in sections" :key="section.id" :href="`#${section.id}`">
+      {{ section.title }}
+    </a>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Name.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup></script>
+
+<template>
+  <h1>Juliette Le Meudec</h1>
+  <p>I am a doctorate at Inria</p>
+</template>
+
+<style scoped></style>
